b80c449f97fe16170c1f58a4ce5c2f3446b6888e
OffsetManager <azikkot.games@gmail.com>
2026-08-01 14:32:05 +0300

    Update Internal_Offsets.hpp

--- Windows/version-145f189a6a974303/Internal_Offsets.hpp
+++ Windows/version-145f189a6a974303/Internal_Offsets.hpp
@@ -2,35 +2,38 @@
 #include "Struct.hpp"
 
 // ------------------------------------------------------------
-//  Dumper   : Argus 1.5.5
-//  Alert    : This is an experimental dumper, so please verify offsets before use.
-//  Owner    : @phantomteam | @kreker757
+//   Dumper   : Argus 2.0.0 (Internal Offsets)
+//   Alert    : This is an experimental dumper, so please verify offsets before use.
+//   Owner    : @phantomteam | @kreker757
 //  Version  : version-145f189a6a974303
 //  Failed   : 0
-//  Success  : 223
-//  Total    : 223
+//  Success  : 156
+//  Total    : 156
 // ------------------------------------------------------------
 
 namespace Offsets
 {
+    const uintptr_t AppdataInfo = REBASE(0x606D1E0);
     const uintptr_t AttachRobloxExtraSpace = REBASE(0x1DB73E0);
-    const uintptr_t GetCapabilities = REBASE(0x215D1F0);
+    const uintptr_t GetCapabilities = REBASE(0x4C2D260);
     const uintptr_t GetContextObject = REBASE(0x1D66C90);
     const uintptr_t GetCurrentThreadId = REBASE(0x5123F60);
     const uintptr_t GetCurrentThreadIdentity = REBASE(0x17D833B);
+    const uintptr_t GetGlobalState = REBASE(0x1D56360);
     const uintptr_t GetIdentityStruct = REBASE(0x4C2CE10);
-    const uintptr_t GetLuaState = REBASE(0x1D56360);
+    const uintptr_t GetLuaState = REBASE(0x1E54CF0);
     const uintptr_t GetModuleFromVMStateMap = REBASE(0x1DEB700);
     const uintptr_t GetProperty = REBASE(0xA1D677D);
     const uintptr_t GetPropertyData = REBASE(0xC8ACA0);
     const uintptr_t GetTLSPointer = REBASE(0x80E0);
     const uintptr_t GetValues = REBASE(0x1D507B0);
     const uintptr_t IdentityPtr = REBASE(0x8392B28);
+    const uintptr_t IentityToCaps = REBASE(0x4C2D260);
     const uintptr_t Impersonator = REBASE(0x512B690);
     const uintptr_t NewInstance = REBASE(0xAB98C0);
     const uintptr_t OpCodeLookupTable = REBASE(0x66BB660);
     const uintptr_t PushInstance = REBASE(0x1D48CA0);
-    const uintptr_t ScriptContextResume = REBASE(0x4D106B0);
+    const uintptr_t ScriptContextResume = REBASE(0x1E21510);
     const uintptr_t SetFFlag = REBASE(0x5172100);
     const uintptr_t cocreate = REBASE(0x4B7E710);
     const uintptr_t coroutine_resume = REBASE(0x1D67CA0);
@@ -38,6 +41,11 @@ namespace Offsets
     const uintptr_t getglobalstate = REBASE(0x1D66A90);
     const uintptr_t loadsafe = REBASE(0x4B6ECE0);
     const uintptr_t print = REBASE(0x4C27BF0);
+
+    namespace reflections
+    {
+        const uintptr_t CastArgs = REBASE(0x54BDA98);
+    }
 
     namespace Luau
     {
@@ -138,18 +146,16 @@ namespace Offsets
     namespace TaskScheduler
     {
         const uintptr_t PhysicsSenderMaxBandwidthBps = REBASE(0x5EE4E20);
-        const uintptr_t RawScheduler = REBASE(0x5FE9EC0);
+        const uintptr_t RawScheduler = REBASE(0x79D5A90);
+        const uintptr_t TaskDelay = REBASE(0x1E95A30);
         const uintptr_t TaskDesynchronize = REBASE(0x1E95C80);
+        const uintptr_t TaskSchedulerPointer = REBASE(0x79D5A90);
         const uintptr_t TaskSchedulerTargetFps = REBASE(0x83A5638);
         const uintptr_t TaskSpawn = REBASE(0x1E96520);
         const uintptr_t TaskSynchronize = REBASE(0x1E966A0);
+        const uintptr_t TaskWait = REBASE(0x1E968A0);
         const uintptr_t task_cancel = REBASE(0x1E95330);
         const uintptr_t task_defer = REBASE(0x1E95640);
-    }
-
-    namespace Actor
-    {
-        constexpr uintptr_t ParallelFlag = 0x50;
     }
 
     namespace Input
@@ -157,7 +163,8 @@ namespace Offsets
         const uintptr_t FireMouseClick = REBASE(0x26ADEE0);
         const uintptr_t FireMouseHoverEnter = REBASE(0x26AE0F0);
         const uintptr_t FireMouseHoverLeave = REBASE(0x26AE2E0);
-        const uintptr_t FireProximityPrompt = REBASE(0x26B7440);
+        const uintptr_t FireProximityPrompt = REBASE(0x26E9EC0);
+        const uintptr_t FireRightMouseClick = REBASE(0x26AE550);
         const uintptr_t FireTouchInterest = REBASE(0x2B25900);
     }
 
@@ -172,39 +179,17 @@ namespace Offsets
         const uintptr_t Send = REBASE(0x324E650);
     }
 
-    namespace DataModel
-    {
-        constexpr uintptr_t GameLoaded = 0x578;
-        constexpr uintptr_t RequireBypass = 0x919;
-    }
-
-    namespace Properties
-    {
-        constexpr uintptr_t ClassDescriptor = 0x18;
-        constexpr uintptr_t ClassName = 0x8;
-        constexpr uintptr_t Properties = 0x3C0;
-        constexpr uintptr_t PropertyDescriptorBitFlags = 0x8C;
-        constexpr uintptr_t TType = 0x68;
-        constexpr uintptr_t TTypeNumber = 0x30;
-    }
-
     namespace Capabilities
     {
-        const uintptr_t GetCapabilities = REBASE(0x215D1F0);
-        constexpr uintptr_t Capabilities = 0x28;
+        const uintptr_t GetCapabilities = REBASE(0x4C2D260);
     }
 
     namespace GarbageCollector
     {
         constexpr uintptr_t L_global = 0x28;
         constexpr uintptr_t L_singlestep = 0x5;
-        constexpr uintptr_t g_freepages = 0x98;
-        constexpr uintptr_t g_gcstate = 0x59;
         constexpr uintptr_t g_gcstepmul = 0x49;
         constexpr uintptr_t g_gcstepsize = 0x4D;
-        constexpr uintptr_t g_gray = 0x20;
-        constexpr uintptr_t g_grayagain = 0x18;
-        constexpr uintptr_t g_weak = 0x10;
     }
 
     namespace Flags
@@ -212,91 +197,9 @@ namespace Offsets
         const uintptr_t GetFFlag = REBASE(0x516C650);
     }
 
-    namespace PropertyDescriptor
-    {
-        constexpr uintptr_t CallbackStorage = 0x80;
-        constexpr uintptr_t PropertyDescriptorBitFlags = 0x8C;
-        constexpr uintptr_t ScriptableMask = 0x10;
-        constexpr uintptr_t TypeDescriptor = 0x68;
-    }
-
-    namespace Callback
-    {
-        constexpr uintptr_t DataModelAdjustment = 0x1C8;
-        constexpr uintptr_t Reference = 0x34;
-        constexpr uintptr_t WeakChain = 0x38;
-        constexpr uintptr_t WeakObject = 0x18;
-        constexpr uintptr_t Wrapper = 0x18;
-    }
-
-    namespace ClassDescriptor
-    {
-        constexpr uintptr_t CallbackEntryKindValue = 0x4;
-        constexpr uintptr_t PropertyEntryDescriptor = 0x0;
-        constexpr uintptr_t PropertyEntryKind = 0x8;
-        constexpr uintptr_t PropertyEntryKindValue = 0x0;
-        constexpr uintptr_t PropertyMap = 0x250;
-        constexpr uintptr_t PropertyMapBucketIndices = 0x2C8;
-        constexpr uintptr_t PropertyMapBucketMask = 0x2D8;
-        constexpr uintptr_t PropertyMapEntries = 0x2D0;
-        constexpr uintptr_t PropertyMapEntrySize = 0x10;
-    }
-
-    namespace BasePart
-    {
-        constexpr uintptr_t Overlap = 0x1F0;
-        constexpr uintptr_t Primitive = 0x128;
-    }
-
-    namespace Render
-    {
-        constexpr uintptr_t RenderJobToView = 0x1D0;
-        constexpr uintptr_t RenderViewToDevice = 0x8;
-    }
-
     namespace SignalConnection
     {
         const uintptr_t WaitScriptSlotVirtualTable = REBASE(0x6031440);
-        constexpr uintptr_t Enabled = 0x20;
-        constexpr uintptr_t FunctionScriptSlotSelf = 0x18;
-        constexpr uintptr_t Next = 0x10;
-        constexpr uintptr_t SelfWeakFallback = 0x48;
-        constexpr uintptr_t SlotWrapper = 0x30;
-        constexpr uintptr_t SlotWrapperFallbackSelf = 0x40;
-        constexpr uintptr_t SlotWrapperSelf = 0x38;
-        constexpr uintptr_t WeakChain = 0x68;
-        constexpr uintptr_t WeakChainNext = 0x8;
-    }
-
-    namespace SignalWeakObject
-    {
-        constexpr uintptr_t FunctionReference = 0x34;
-        constexpr uintptr_t Pointer = 0x60;
-        constexpr uintptr_t Thread = 0x28;
-        constexpr uintptr_t WaitContainer = 0x50;
-    }
-
-    namespace EventInstance
-    {
-        constexpr uintptr_t Descriptor = 0x0;
-        constexpr uintptr_t OwningInstance = 0x18;
-    }
-
-    namespace EventDescriptor
-    {
-        constexpr uintptr_t Signature = 0x48;
-    }
-
-    namespace EventSignature
-    {
-        constexpr uintptr_t FirstParameter = 0x8;
-        constexpr uintptr_t MaximumParameters = 0x80;
-        constexpr uintptr_t ParameterStride = 0x70;
-    }
-
-    namespace VirtualTable
-    {
-        constexpr uintptr_t RaiseEventInvocation = 0x18;
     }
 
     namespace VTables
@@ -324,7 +227,7 @@ namespace Offsets
 
     namespace Atom
     {
-        const uintptr_t KTable = REBASE(0x83A7300);
+        const uintptr_t KTable = REBASE(0x85A7390);
     }
 
     namespace Task
@@ -336,12 +239,6 @@ namespace Offsets
 
     namespace Script
     {
-        const uintptr_t ScriptContextResume = REBASE(0x4D106B0);
-    }
-
-    namespace Hyperion
-    {
-        const uintptr_t InstrumentationCallback = REBASE(0x14F950);
-        const uintptr_t RobloxEntry = REBASE(0x54BE1F8);
+        const uintptr_t ScriptContextResume = REBASE(0x1E21510);
     }
 }
