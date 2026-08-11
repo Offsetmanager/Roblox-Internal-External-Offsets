b2f80ba92f6c424f1ed54dc51c1de6b93e25b3f2
OffsetManager <azikkot.games@gmail.com>
2026-08-11 15:55:09 +0300

    Create Struct.hpp

---
+++ Windows/version-2269b67864ab4ad4/Struct.hpp
@@ -0,0 +1,14 @@
+#define ProtoLineinfoEnc      VMValue3
+#define ProtoLocvarsEnc       VMValue1
+#define ProtoUpvaluesEnc      VMValue1
+#define ProtoDebuginsnEnc     VMValue2
+#define ProtoTypeinfoEnc      VMValue1
+#define ProtoDebugnameEnc     VMValue4
+#define ProtoSourceEnc        VMValue1
+#define ProtoUserdataEnc      VMValue4
+#define ProtoAbslineinfoEnc   VMValue4
+#define ClosureDebugNameEnc   VMValue1
+#define LuaStateStackSizeEnc  VMValue4
+#define ClosureContEnc        VMValue3
+#define UdataMetaEnc          VMValue3
+#define TStringHashEnc        VMValue3
