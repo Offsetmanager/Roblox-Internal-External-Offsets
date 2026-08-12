3dcf56beeab6389cefadb33a331e409337a3bc72
OffsetManager <azikkot.games@gmail.com>
2026-08-12 08:47:50 +0300

    Create Internal_Offsets.hpp

---
+++ Windows/version-ddf602d9cfe44005/Internal_Offsets.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <cstdint>
+
+// ------------------------------------------------------------
+//  Dumper   : ArgusX Beta 1.0
+//  Success  : 10
+//  Failed   : 0
+// ------------------------------------------------------------
+
+#ifndef REBASE
+#define REBASE(x) (x)
+#endif
+
+namespace Offsets
+{
+    const uintptr_t Print = REBASE(0x92C340);
+    const uintptr_t ScriptContextResume = REBASE(0x22BBA10);
+    const uintptr_t GetLuaStateForInstance = REBASE(0x7B3D9E);
+    const uintptr_t luaD_throw = REBASE(0xB40C30);
+
+    namespace Luau
+    {
+        const uintptr_t LuaO_NilObject = REBASE(0x610EFF8);
+        const uintptr_t LuaH_DummyNode = REBASE(0x610EEC0);
+    }
+
+    namespace ExtraSpace
+    {
+        const uintptr_t RequireBypass = 0x9D0;
+    }
+
+    namespace ScriptContext
+    {
+        const uintptr_t RequireBypass = 0x840;
+        const uintptr_t RequireBypassLegacy = 0x9D0;
+    }
+
+    namespace Capabilities
+    {
+        const uintptr_t GetCapabilities = REBASE(0x8F7FD0);
+    }
+}
