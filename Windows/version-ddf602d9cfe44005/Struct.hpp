a4b48a5d2098ceb5cf2859d0207500fc3db32afa
OffsetManager <azikkot.games@gmail.com>
2026-08-13 20:49:46 +0300

    Create Struct.hpp

---
+++ Windows/version-ddf602d9cfe44005/Struct.hpp
@@ -0,0 +1,22 @@
+// ------------------------------------------------------------
+//  Dumper      : Argus 3.0 (Struct)
+//  Owner       : @phantomteam | @kreker757
+//  Version     : version-ddf602d9cfe44005
+//  Time Taken  : 52s
+// ------------------------------------------------------------
+#pragma once
+
+#define ProtoLineinfoEnc VMValue3
+#define ProtoLocvarsEnc VMValue1
+#define ProtoUpvaluesEnc VMValue1
+#define ProtoDebuginsnEnc VMValue2
+#define ProtoTypeinfoEnc VMValue1
+#define ProtoDebugnameEnc VMValue4
+#define ProtoSourceEnc VMValue1
+#define ProtoUserdataEnc VMValue4
+#define ProtoAbslineinfoEnc VMValue4
+#define ClosureDebugNameEnc VMValue1
+#define LuaStateStackSizeEnc VMValue4
+#define ClosureContEnc VMValue3
+#define UdataMetaEnc VMValue3
+#define TStringHashEnc VMValue3
