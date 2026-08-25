8ebb08121fee060996e9feb49f73220c96097132
OffsetManager <azikkot.games@gmail.com>
2026-08-25 12:56:45 +0300

    Update Struct.hpp

--- Windows/version-37a6cf8c67114d6d/Struct.hpp
+++ Windows/version-37a6cf8c67114d6d/Struct.hpp
@@ -3,14 +3,21 @@
 //  Alert       : This is an experimental dumper, so please verify offsets before use.
 //  Owner       : @phantomteam | @kreker757
 //  Version     : version-37a6cf8c67114d6d
-//  Time Taken  : 54s
+//  Time Taken  : 57s
 // ------------------------------------------------------------
 #pragma once
 
 #define PROTO_LINEINFO_ENC VMValue3
-#define PROTO_LOCVARS_ENC VMValue4
-#define PROTO_UPVALUES_ENC VMValue1
-#define PROTO_DEBUGINSN_ENC VMValue2
-#define PROTO_TYPEINFO_ENC VMValue4
+#define PROTO_LOCVARS_ENC VMValue3
+#define PROTO_UPVALUES_ENC VMValue4
+#define PROTO_DEBUGINSN_ENC VMValue1
+#define PROTO_TYPEINFO_ENC VMValue2
+#define PROTO_DEBUGNAME_ENC VMValue4
+#define PROTO_SOURCE_ENC VMValue1
+#define PROTO_USERDATA_ENC VMValue4
+#define PROTO_ABSLINEINFO_ENC VMValue4
 #define CLOSURE_DEBUGNAME_ENC VMValue1
+#define LSTATE_STACKSIZE_ENC VMValue1
+#define CLOSURE_CONT_ENC VMValue3
+#define UDATA_META_ENC VMValue3
 #define TSTRING_HASH_ENC VMValue1
