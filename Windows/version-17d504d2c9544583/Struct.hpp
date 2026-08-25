1515e812c9342ef373658b51116565769a48f36e
OffsetManager <azikkot.games@gmail.com>
2026-08-25 21:36:45 +0300

    Create Struct.hpp

---
+++ Windows/version-17d504d2c9544583/Struct.hpp
@@ -0,0 +1,23 @@
+// ------------------------------------------------------------
+//  Dumper      : Argus 3.0 (Struct)
+//  Alert       : This is an experimental dumper, so please verify offsets before use.
+//  Owner       : @phantomteam | @kreker757
+//  Version     : version-17d504d2c9544583
+//  Time Taken  : 48s
+// ------------------------------------------------------------
+#pragma once
+
+#define PROTO_LINEINFO_ENC VMValue3
+#define PROTO_LOCVARS_ENC VMValue2
+#define PROTO_UPVALUES_ENC VMValue1
+#define PROTO_DEBUGINSN_ENC VMValue4
+#define PROTO_TYPEINFO_ENC VMValue4
+#define PROTO_DEBUGNAME_ENC VMValue2
+#define PROTO_SOURCE_ENC VMValue4
+#define PROTO_USERDATA_ENC VMValue4
+#define PROTO_ABSLINEINFO_ENC VMValue4
+#define CLOSURE_DEBUGNAME_ENC VMValue4
+#define LSTATE_STACKSIZE_ENC VMValue2
+#define CLOSURE_CONT_ENC VMValue3
+#define UDATA_META_ENC VMValue3
+#define TSTRING_HASH_ENC VMValue1
