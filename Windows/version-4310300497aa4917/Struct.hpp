290d5f368a074d638864faac1e3353ebc608a169
OffsetManager <azikkot.games@gmail.com>
2026-09-19 13:41:08 +0300

    Update Struct.hpp

--- Windows/version-4310300497aa4917/Struct.hpp
+++ Windows/version-4310300497aa4917/Struct.hpp
@@ -3,8 +3,8 @@
 //  Alert       : Verify offsets before use.
 //  Owner       : @phantomteam | @kreker757
 //  Version     : version-4310300497aa4917
-//  Time Taken  : 50.91s
-//  Success     : 5
+//  Time Taken  : 141.35s
+//  Success     : 9
 //  Failed      : 0
 // ------------------------------------------------------------
 #pragma once
@@ -12,5 +12,9 @@
 #define PROTO_LINEINFO_ENC VMValue2
 #define PROTO_LOCVARS_ENC VMValue4
 #define PROTO_UPVALUES_ENC VMValue3
-#define UDATA_META_ENC VMValue3
+#define PROTO_DEBUGINSN_ENC VMValue4
+#define PROTO_DEBUGNAME_ENC VMValue4
+#define PROTO_TYPEINFO_ENC VMValue2
+#define CLOSURE_DEBUGNAME_ENC VMValue1
 #define TSTRING_HASH_ENC VMValue3
+#define UDATA_META_ENC VMValue3
